b6b570c22aacaeb13f0b82bf6c89aa9e742f94af
mushkhushu <flavie.houlette2023@campus-eni.fr>
2024-12-12 16:55:15 +0100

    add nesting of components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from "./components/home/home.component";
+import {LoginComponent} from "./components/login/login.component";
+import {DetailsComponent} from "./components/details/details.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: LoginComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'logout', component: LoginComponent},
+  {path: 'details/:id', component: DetailsComponent},
+
+];

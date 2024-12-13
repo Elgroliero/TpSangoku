fd2f841d1199bb1f654d5199b649066fdf1983af
mushkhushu <flavie.houlette2023@campus-eni.fr>
2024-12-13 14:59:56 +0100

    ajouter un fichier constants commun avec la base_url et l'enum de races

---
+++ src/app/constants/constants.ts
@@ -0,0 +1,17 @@
+export const BASE_URL = "https://dragonball-api.com/api";
+
+export enum Race {
+  Saiyan = "Saiyan",
+  Namekian = "Namekian",
+  Human = "Human",
+  Majin = "Majin",
+  FriezaRace = "Frieza Race",
+  JirenRace = "Jiren Race",
+  Android = "Android",
+  God = "God",
+  Angel = "Angel",
+  Evil = "Evil",
+  Unknown = "Unknown",
+  NucleicoBenigno = "Nucleico benigno",
+  Nucleico = "Nucleico"
+}
--- src/app/repositories/character-repository.service.ts
+++ src/app/repositories/character-repository.service.ts
@@ -1,14 +1,15 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Character} from "../models/character";
-import {catchError, map, Observable, retry, tap, throwError, timeout} from "rxjs";
+import {map, Observable} from "rxjs";
+import {BASE_URL} from '../constants/constants';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CharacterRepositoryService {
 
-  private readonly BASE_URL = "https://dragonball-api.com/api"
+  private readonly BASE_URL = BASE_URL;
 
   constructor(private http: HttpClient) {
   }
